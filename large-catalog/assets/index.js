4a827e9cd02c6d217a14ee33913440e4e91425d2
thecoorum <coorum.y@gmail.com>
2022-05-10 15:21:24 +0300

    Add products section and carousel

---
+++ large-catalog/assets/index.js
@@ -0,0 +1,50 @@
+window.addEventListener("load", () => {
+  document.querySelectorAll(".bq-carousel").forEach((carousel) => {
+    const inner = carousel.querySelector(".bq-carousel__inner");
+    const prev = carousel.querySelector(".bq-carousel__prev");
+    const next = carousel.querySelector(".bq-carousel__next");
+
+    new Glider(inner, {
+      slidesToShow: "auto",
+      slidesToScroll: 1,
+      scrollLock: true,
+      itemWidth: 212,
+      exactWidth: true,
+      arrows: {
+        prev,
+        next,
+      },
+      responsive: [
+        {
+          breakpoint: 810,
+          settings: {
+            slidesToShow: "auto",
+            slidesToScroll: 2,
+          },
+        },
+        {
+          breakpoint: 1200,
+          settings: {
+            slidesToShow: 4,
+            slidesToScroll: 2,
+            itemWidth: 312,
+          },
+        },
+      ],
+    });
+  });
+
+  // Firefox hide scrollbar workaround
+  document.addEventListener("glider-loaded", hideFFScrollBars);
+  document.addEventListener("glider-refresh", hideFFScrollBars);
+
+  const hideFFScrollBars = (e) => {
+    var scrollbarHeight = 17;
+    if (/firefox/i.test(navigator.userAgent)) {
+      if (window.innerWidth > 575) {
+        e.target.parentNode.style.height =
+          e.target.offsetHeight - scrollbarHeight + "px";
+      }
+    }
+  };
+});
