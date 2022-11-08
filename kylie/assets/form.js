4aa4fe768061c12501c7ef4dae0e070fcc25de1c
Yaroslav <coorum.y@gmail.com>
2022-11-08 16:27:38 +0200

    Update script attribute to automatically gather data

--- kylie/assets/form.js
+++ kylie/assets/form.js
@@ -1,3 +1,15 @@
-const onFormSubmit = (e) => {
-  console.log(e)
-}
+const forms = document.querySelectorAll("form");
+
+const handleSubmit = ({ event, form }) => {
+  event.preventDefault();
+
+  const formData = new FormData(form);
+
+  const data = Object.fromEntries(formData);
+
+  console.log(data);
+};
+
+forms.forEach((form) => {
+  form.addEventListener("submit", (e) => handleSubmit({ event: e, form }));
+});
